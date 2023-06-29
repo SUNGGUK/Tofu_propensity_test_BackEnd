7e2557aeadd56cbea5efafadfda56272460c8b60
dudxo <father8475@naver.com>
2023-06-30 00:44:49 +0900

    test

---
+++ 영태/src/test/java/com/example/tofu_test/TofuTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tofu_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TofuTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
